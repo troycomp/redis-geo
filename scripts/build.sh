4e353a0ce58ba8d27972da26a32db7309aa50bc6
Troy Compton <troy.compton@gmail.com>
2024-03-18 04:02:24 -0500

    echo

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ source ../deploy-envs.sh
 
 #AWS_ACCOUNT_NUMBER={} set in private variable
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-
+echo "docker for aws $AWS_ECS_REPO_DOMAIN"
 # Build process
 docker build -t $IMAGE_NAME ../
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
